4dc671a1c2e003fed6ec8eade3b96c5c84c5ec28
BaiDom <dombaily88@gmail.com>
2024-11-17 22:40:36 +0000

    added 5day forecast functionality, slight css update

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -12,6 +12,8 @@ import rain_icon from "../assets/rain.png";
 const Weather = () => {
   const inputRef = useRef();
   const [weatherData, setWeatherData] = useState(false);
+  const [forecastData, setForecastData] = useState([]);
+  const [showForecast, setShowForecast] = useState(false);
   const allIcons = {
     "01d": clear_icon,
     "01n": clear_icon,
@@ -27,6 +29,11 @@ const Weather = () => {
     "10n": rain_icon,
     "13d": snow_icon,
     "13n": snow_icon,
+  };
+
+  const handleCitySearch = (city) => {
+    search(city);
+    forecast(city);
   };
 
   const search = async (city) => {
@@ -47,7 +54,6 @@ const Weather = () => {
         return;
       }
 
-      console.log(data);
       const icon = allIcons[data.weather[0].icon] || clear_icon;
       setWeatherData({
         humidity: data.main.humidity,
@@ -71,11 +77,64 @@ const Weather = () => {
     }
   };
 
+  const forecast = async (city) => {
+    if (city === "") {
+      alert("Enter a city name");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const res = await fetch(url);
+      const forecasted = await res.json();
+
+      if (!res.ok) {
+        alert(forecasted.message);
+        return;
+      }
+
+      const forecastedList = forecasted.list;
+
+      const processWeatherData = (forecastedList) => {
+        return forecastedList.map((item) => {
+          const date = new Date(item.dt * 1000);
+          const dateDay = date.toDateString().split(" ").slice(0, 1).join(" ");
+          const dateNum = date.toDateString().split(" ").slice(2, 3).join(" ");
+          console.log(
+            date.toDateString().split(" ").slice(2, 3).join(" "),
+            "date string"
+          );
+          return {
+            date: dateDay + " " + dateNum,
+            time: date.toTimeString().split(" ")[0],
+            temperature: item.main.temp,
+            weather: item.weather[0].main,
+            description: item.weather[0].description,
+            windSpeed: item.wind.speed,
+            humidity: item.main.humidity,
+          };
+        });
+      };
+
+      const processedData = processWeatherData(forecasted.list);
+
+      setForecastData(processedData);
+    } catch (error) {
+      console.log(error);
+      setForecastData(false);
+      console.error(
+        "There was an error whilst trying to find the weather data"
+      );
+    }
+  };
+
   let dateString = Date(weatherData.date).toLocaleString();
   let trimDate = dateString.split(" ").slice(0, 4).join(" ");
 
   useEffect(() => {
-    search("Liverpool");
+    handleCitySearch("Liverpool");
   }, []);
 
   return (
@@ -89,34 +148,75 @@ const Weather = () => {
           placeholder="Search"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              search(inputRef.current.value);
+              handleCitySearch(inputRef.current.value);
             }
           }}
         ></input>
         <img
           src={search_icon}
           alt=""
-          onClick={() => search(inputRef.current.value)}
+          onClick={() => handleCitySearch(inputRef.current.value)}
         />
       </div>
       {weatherData ? (
         <>
-          <img src={weatherData.icon} alt="" className="weather-icon" />
           <p className="location">{weatherData.location}</p>
-          <p className="weather-main">{weatherData.weatherMain}</p>
-          {/* <p className="weather-desc">({weatherData.weatherDesc})</p> */}
           <p className="temp">{weatherData.temperature}°C</p>
-          <div className="temp-data">
-            <div className="td-item feels-like">
-              <p>Feels Like</p> {weatherData.feelsLike}°C
-            </div>
-            <div className="td-item min-temp">
-              <p>Min Temp</p> {weatherData.minTemp}°C
-            </div>
-            <div className="td-item max-temp">
-              <p>Max Temp</p> {weatherData.maxTemp}°C
-            </div>
+          <img src={weatherData.icon} alt="" className="weather-icon" />
+          <p className="weather-main">{weatherData.weatherMain}</p>
+          <div
+            id="forecast-button"
+            onClick={() => setShowForecast(!showForecast)}
+          >
+            {showForecast && <div>Today</div>}
+            {!showForecast && <div>5 day forecast</div>}
           </div>
+          {showForecast ? (
+            forecastData.length > 0 && (
+              <div className="forecast">
+                {forecastData.map(
+                  (forecast, index) =>
+                    forecast.time === "12:00:00" && (
+                      <div key={index} className="five-day-div">
+                        <p className="forecast-date">{forecast.date}</p>
+                        {/* <p>Time: {forecast.time}</p> */}
+                        <p>{Math.round(forecast.temperature)}°C</p>
+                        <p>{forecast.weather}</p>
+                        <p className="five-day-hum">
+                          <img
+                            className="five-day-hum-img"
+                            src="/src/assets/humidity.png"
+                            alt=""
+                          />
+                          {forecast.humidity}%
+                        </p>
+                        {/* <p className="five-day-wind">
+                          <img
+                            className="five-day-wind-img"
+                            src="/src/assets/wind.png"
+                            alt=""
+                          />
+                          {forecast.windSpeed} Km/h
+                        </p> */}
+                      </div>
+                    )
+                )}
+              </div>
+            )
+          ) : (
+            <div className="forecast forecast-td">
+              <div className="td-item feels-like">
+                <p>Feels Like</p> {weatherData.feelsLike}°C
+              </div>
+              <div className="td-item min-temp">
+                <p>Min Temp</p> {weatherData.minTemp}°C
+              </div>
+              <div className="td-item max-temp">
+                <p>Max Temp</p> {weatherData.maxTemp}°C
+              </div>
+            </div>
+          )}
+
           <div className="weather-data">
             <div className="col">
               <img src={humidity_icon} alt="" />
